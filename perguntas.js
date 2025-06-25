30728a7bfe03a1d98b3b4e5cf3750c7127c71f6d
anaaflavia2 <ana.renosto.flavia@gmail.com>
2025-06-25 15:56:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'História',
+    'O que foi Imperialismo no séculoXlX?',
+    'Expansão das potencias europeias sobre a Africa, Asia e America Latina em busca de recursos e mercados'
+)
+
+criaCartao(
+    'História',
+    'Quais são as princiapos potencias do Imperialismo no séculoXlX?',
+    'Grã-Bretanha, França, Alemanha, Belgica, Portugal e Italia'
+)
+
+criaCartao(
+    'História',
+    'Qual foi a motivação economica do Imperialismo no séculoXlX?',
+    'Necessidades de materias-primas e novos mercados para produtos industrializados'
+)
+
+criaCartao(
+    'História',
+    'Qual foi as formas de resistencia no Imperialismo no séculoXlX?',
+    'Revoltas armadas, movimentos nacionalistas e aliança entre grupos locais'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o impacto nas populações que o Imperialismo no séculoXlX causou?',
+    'Exploração, perda de terras, imposto cultural'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o impacto na Africa?',
+    'Divisão da Africa em colonias sem considerar fronteiras eticas gerando conflitos internos'
+)
